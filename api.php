6b493ab213c46d933cb6e7bdcf3ad4858a6276d8
Aminu M. Bulangu <42899653+blgguy@users.noreply.github.com>
2020-05-09 21:03:39 +0100

    Create api.php

---
+++ api.php
@@ -0,0 +1,192 @@
+<?php
+/**
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+**/
+
+$data = file_get_contents('https://api.thevirustracker.com/free-api?countryTotal=NG');
+//$arrayName = array('case' => 679, );
+/**
+$data = '{
+	"countrydata":[{
+		"info":{
+			"ourid"		:	116,
+			"title"		:	"Nigeria",
+			"code"		:	"NG",
+			"source"	:"https://thevirustracker.com/nigeria-coronavirus-information-ng" 
+		},
+		"total_cases"			:	1728,
+		"total_recovered"		:	307,
+		"total_unresolved"		:	0,
+		"total_deaths"			:	51,
+		"total_new_cases_today"	:	0,
+		"total_new_deaths_today"	:	0,
+		"total_active_cases"	:	1370,
+		"total_serious_cases"	:	2,
+		"total_danger_rank"		:	71
+}],
+		"stat" : "ok"}';
+		//$data = json_encode($data);
+		$data = str_replace("[", "", $data);
+		$data = str_replace("]", "", $data);
+		//var_dump($data);
+		/** **/
+
+		$data = str_replace("[", "", $data);
+
+		$data = str_replace("]", "", $data);
+$arr = json_decode($data, true);
+//$arr = json_decode($data);
+$CountryName = $arr['countrydata']['info']['title'];
+$CountryCode = $arr['countrydata']['info']['code'];
+
+$CountryToatalCases = $arr['countrydata']['total_cases'];
+$CountryToatalRecovered = $arr['countrydata']['total_recovered'];
+$CountryToatalDeath = $arr['countrydata']['total_deaths'];
+$CountryNewCasesToday = $arr['countrydata']['total_new_cases_today'];
+$CountryNewDeathsToday = $arr['countrydata']['total_new_deaths_today'];
+$CountryActiveCases = $arr['countrydata']['total_active_cases'];
+
+echo "<b>".$CountryToatalCases."<b>";
+**/
+
+/** For all country.. 
+
+
+
+$data2 = '{
+	"result":[
+	{
+		"total_cases"			:	3230433,
+		"total_recovered"		:	1007136,
+		"total_unresolved"		:	2039891,
+		"total_deaths"			:	228394,
+		"total_new_cases_today"	:	12249,
+		"total_new_deaths_today"	:	364,
+		"total_active_cases"	:	1994903,
+		"total_serious_cases"	:	59800,
+		"total_danger_rank"		:	210,
+		"source":{
+			"url"	:	"https://thevirustracker.com"
+		}
+	}],
+		"stat" : "ok"}';
+
+		$data2 = str_replace("[", "", $data2);
+		$data2 = str_replace("]", "", $data2);
+		//var_dump($data);
+		/** **/
+$data2 = file_get_contents('https://thevirustracker.com/free-api?global=stats');
+
+		$data2 = str_replace("[", "", $data2);
+		$data2 = str_replace("]", "", $data2);
+$arr2 = json_decode($data2, true);
+
+$GlobalToatalCases = $arr2['results']['total_cases'];
+$GlobalSource = $arr2['results']['source']['url'];
+$GlobalToatalRecovered = $arr2['results']['total_recovered'];
+$GlobalToatalDeaths = $arr2['results']['total_deaths'];
+$GlobalNewCasesToday = $arr2['results']['total_new_cases_today'];
+$GlobalDeathToday = $arr2['results']['total_new_deaths_today'];
+$GlobalActiveCases = $arr2['results']['total_active_cases'];
+
+echo "<br>".$GlobalSource."<br>$status<br>".$GlobalToatalCases;
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="inc/style.css">
+	<link rel="stylesheet" type="text/css" href="inc/css/bootstrap.min.css">
+	<title></title>
+	<style type="text/css">
+		#all{
+			height: 100%;
+			width: 100%;
+			background-color: red; 
+		}
+
+		#triangle_to_right{
+			/**float: righ **/t;
+			width: 0;
+			height: 0;
+			border-top: 100px solid darkgreen;
+			border-left: 100px solid transparent; 
+		}
+
+		#triangle_to_left{
+			/**float: left **/
+			width: 0;
+			height: 0;
+			border-top: 100px solid darkgreen;
+			border-right: 100px solid transparent; 
+		}
+
+		#tab1{
+			position: relative;
+			width: 200;
+			height: 150px;
+			margin: 20px 0;
+			background-color: darkgreen;
+			border-radius: 50% / 10% ;
+			text-align: center;
+			text-indent: 3em;
+		}
+		#tab1:before{
+			content: '';
+			position: absolute;
+			top: 100%;
+			bottom: 10%;
+			right: -5%;
+			left: -5%;
+			background: inherit;
+			border-radius: 5% / 50%;
+		}
+		
+	</style>
+</head>
+<body>
+
+<div  id="captioned-gallery">
+	<figure class="slider">
+		<figure>
+			<img src="inc/img/1.png" alt="Nigeria Covid-19 Tracking">
+			<figcaption>
+					Nigeria
+					Total Cases : <b><?php echo $CountryToatalCases;?></b>,<br>
+					Total Recovered : <b><?php echo $CountryToatalRecovered;?> </b><br>
+					Total Deaths : <b><?php echo $CountryToatalDeath;?> </b><br>
+					Total New Cases Today : <b><?php echo $CountryNewCasesToday;?> </b><br>		
+					Total New Deaths Today : <b><?php echo $CountryNewDeathsToday;?> </b><br>	
+					Total Active Cases : <b><?php echo $CountryActiveCases;?> </b>
+			</figcaption>
+		</figure>
+	</figure>
+</div>
+<!-- Colomn start 50% wide on mobile and bump up to 33.3% wide on desktop-->
+<div class="container">
+	<h3>Global Data</h3>
+	<div class="row">
+		<div class="col-4 col-md-4">Total Cases<br><b> <?php echo $GlobalToatalCases; ?></b></div>
+		<div class="col-4 col-md-4">Total Recovered<br><?php echo $GlobalToatalRecovered; ?><b> </b></div>
+		<div class="col-4 col-md-4">Total Deaths<br><b> <?php echo $GlobalToatalDeaths; ?></b></div>
+	</div>
+	<div class="row">
+		<div class="col-4 col-md-4">New Cases Today <br><b> <?php echo $GlobalNewCasesToday; ?></b></div>
+		<div class="col-4 col-md-4">New Deaths Today <br><?php echo $GlobalDeathToday; ?><b> </b></div>
+		<div class="col-4 col-md-4">Active Cases <br><b> <?php echo $GlobalActiveCases; ?></b></div>
+	</div>
+</div>
+
+	<!--div id="all">
+		<div id="triangle_to_right"></div>
+		<div id="triangle_to_left"></div>
+		<div id="tab1"></div>
+		<div id="tab1">Kano</div>
+	</div-->
+
+<footer>
+	<h5>Data generted from </h5>
+</footer>
+</body>
+</html>
